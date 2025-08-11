71716d7ca195a532f9b9adeaf25d29999daca6a1
dhananjaythakre <101576913+dhananjaythakre@users.noreply.github.com>
2025-08-11 17:19:24 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import AdminLayout from './components/layouts/AdminLayout';
+import Login from './components/Login';
+import Dashboard from './components/views/Dashboard'; // Dashboard.jsx is in src/components/views/
+
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<Login />} />
+          <Route path="/Layout" element={<AdminLayout />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
